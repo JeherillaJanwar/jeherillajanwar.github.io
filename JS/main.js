584664084b243bb80442ed2c86448999c5d1fc28
Ishaan Sharma <ishaan328069@gmail.com>
2022-12-16 16:30:25 -0500

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -83,7 +83,7 @@ function notifyRobot(ip) {
     if (ip == "" || ip == undefined) {
         params = {
             username: "My Webhook Name",
-            avatar_url: "../images/ishaan.jpg",
+            avatar_url: "",
             content: `Someone visited your jeherillajanwar.github.io/ site`
         }
     }
