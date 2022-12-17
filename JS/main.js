917a6a16978b18d0dafd4a5906f6f74f7d376dce
Ishaan Sharma <ishaan328069@gmail.com>
2022-12-16 21:16:54 -0500

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -78,13 +78,13 @@ function notifyRobot(ip) {
     var params = {
         username: ip,
         avatar_url: "",
-        content: `<@!JeherillaJanwar!> Someone visited your jeherillajanwar.github.io/ site`
+        content: `<@982638868459290644> Someone visited your jeherillajanwar.github.io/ site`
     }
     if (ip == "" || ip == undefined) {
         params = {
             username: "My Webhook Name",
             avatar_url: "",
-            content: `<@!JeherillaJanwar!> Someone visited your jeherillajanwar.github.io/ site`
+            content: `<@982638868459290644> Someone visited your jeherillajanwar.github.io/ site`
         }
     }
     const request = new XMLHttpRequest();
